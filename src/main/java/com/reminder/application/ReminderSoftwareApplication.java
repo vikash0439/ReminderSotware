2facf5ecd27c16d3d2de99c13d5564d8a6ac5c44
Vikash Kumar <vikash.k@dcmtech.com>
2019-02-07 15:33:10 +0530

    First commit

---
+++ src/main/java/com/reminder/application/ReminderSoftwareApplication.java
@@ -0,0 +1,14 @@
+package com.reminder.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderSoftwareApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/reminder/application/ReminderSoftwareApplicationTests.java
@@ -0,0 +1,17 @@
+package com.reminder.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReminderSoftwareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
